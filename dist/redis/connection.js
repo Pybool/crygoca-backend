9fa51bc2469d0629cb194cd4e20cf05a31cc4d75
Pybool <ekoemmanueljavl@gmail.com>
2024-06-10 17:26:30 +0100

    Fixed live currencies exchange

--- dist/redis/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    generic: {
-        host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_GENERIC_PORT || '6379'),
-        password: process.env.REDIS_PASSWORD
-    }
-};
